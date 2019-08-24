659077e4736563689b139755a845facaffae9eea
simba <simba3310@gmail.com>
2019-08-23 22:45:51 -0500

    season display func & lifecycle comp

--- seasonsapp/src/App.js
+++ seasonsapp/src/App.js
@@ -1,22 +1,36 @@
 import React from "react";
-
+import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            latitude : null
-        }
+  state = {
+    latitude: null,
+    errMessage: ""
+  };
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      pos =>
+        this.setState({
+          latitude: pos.coords.latitude
+        }),
+      err =>
+        this.setState({
+          errorMessage: err.message
+        })
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("component was updated & has re-rendered");
+  }
+
+  render() {
+    if (!this.state.errorMessage && this.state.latitude) {
+      return <div><SeasonDisplay latitude={this.state.latitude}/></div>;
     }
-
-    render(){
-        window.navigator.geolocation.getCurrentPosition(
-            pos => console.log(pos.coords.latitude),
-            err => console.error(err)
-        );
-        return(<div>Latitude :</div>)
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>Error : {this.state.errorMessage}</div>;
     }
-
-
-};
+    return <div>Loading ....</div>;
+  }
+}
 
 export default App;
--- seasonsapp/src/SeasonDisplay.js
+++ seasonsapp/src/SeasonDisplay.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
+const determineSeason =(lat, month) =>{
+    return (month > 2 && month < 9 && lat > 0) ? 'Summer' : 'Winter'
+}
 
-const SeasonDisplay = () => {
-return(<div></div>)
+const SeasonDisplay = (props) => {
+    const season = determineSeason(props.latitude, new Date().getMonth())
+return(<div>{season}</div>)
 }
 
 export default SeasonDisplay;
