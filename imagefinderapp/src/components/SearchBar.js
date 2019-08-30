bdac5eaaa3c14256e86cb4bc6991b5a05fb6fb4e
simba <simba3310@gmail.com>
2019-08-29 19:27:41 -0500

    contextual binding

--- imagefinderapp/src/components/SearchBar.js
+++ imagefinderapp/src/components/SearchBar.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 class searchBar extends React.Component {
-    handleInputChange (e) {
-        e.preventDefault()
-        console.log(e.target.value)
-    }
-    handleInputClick(){
-        console.log("input was clicked!")
-    }
+state = {
+    searchTerm : ''
+}
+handleFormInput(e){
+    e.preventDefault();
+    console.log(this.state.searchTerm)
+}
     render(){
         return(
             <div className={'ui segment' }>
-                <form className={'ui form'}>
+                <form className={'ui form'} onChange={this.handleFormInput}>
                     <div className={'field'}>
                         <label>Image Finder</label>
                         <div className="ui icon input">
                             <i className="search icon"></i>
-                            <input type="text" placeholder="Search..." onChange={this.handleInputChange} onClick={this.handleInputClick}/>
+                            <input type="text" placeholder="Search..." onChange={(e)=>this.setState({searchTerm:e.target.value})} value={this.state.value}/>
                         </div>
                         </div>
                 </form>
