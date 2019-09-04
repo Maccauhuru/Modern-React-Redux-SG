7966908822d4079922537916724d01345e03eee6
simba <simba3310@gmail.com>
2019-09-03 19:54:34 -0500

    pass props child to parent

--- imagefinderapp/src/components/SearchBar.js
+++ imagefinderapp/src/components/SearchBar.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class searchBar extends React.Component {
-state = {
-    searchTerm : ''
-}
-handleFormInput(e){
-    e.preventDefault();
-    console.log(this.state.searchTerm)
-}
-    render(){
-        return(
-            <div className={'ui segment' }>
-                <form className={'ui form'} onChange={this.handleFormInput}>
+class searchBar extends Component {
+    state = {
+        searchTerm: '',
+        status: true
+    };
+
+    onFormSubmit = (e) => {
+        e.preventDefault(e);
+        this.props.onSubmit(this.state.searchTerm)
+    };
+
+
+    render() {
+        return (
+            <div className={'ui segment'}>
+                <form className={'ui form'} onSubmit={this.onFormSubmit}>
                     <div className={'field'}>
                         <label>Image Finder</label>
                         <div className="ui icon input">
                             <i className="search icon"></i>
-                            <input type="text" placeholder="Search..." onChange={(e)=>this.setState({searchTerm:e.target.value})} value={this.state.value}/>
+                            <input type="text" placeholder="Search..."
+                                onChange={(e) => this.setState({ searchTerm: e.target.value })}
+                                value={this.state.searchTerm} />
                         </div>
-                        </div>
+                    </div>
                 </form>
             </div>
         )
     }
 }
+
 
 export default searchBar;
