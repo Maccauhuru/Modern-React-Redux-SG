644c663f8c812ce370306eabec0943da164a09bf
simba <simba3310@gmail.com>
2019-09-28 18:24:45 -0500

    seasons revisited

--- components/src/App.js
+++ components/src/App.js
@@ -1,30 +1,12 @@
 import React, { Component } from "react";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import CardApproval from './CardApproval'
 
 class App extends Component {
   render() {
     return (
-      <div className="ui container comments">
-        <CommentDetail
-          name={"Simon"}
-          time={"Today at 11:30PM"}
-          avatar={faker.image.avatar()}
-          text={"Lets get going!"}
-        />
-        <CommentDetail
-          name={"Wolly"}
-          time={"Yesterday at 8:30AM"}
-          avatar={faker.image.avatar()}
-          text={"Trippy to live"}
-        />
-        <CommentDetail
-          name={"Stunner"}
-          time={"Today at 1:30PM"}
-          avatar={faker.image.avatar()}
-          text={"JSX is ...."}
-        />
-      </div>
+      <CardApproval />
     );
   }
 }
--- seasonsapp/src/App.js
+++ seasonsapp/src/App.js
@@ -1,47 +1,19 @@
 import React from "react";
-import SeasonDisplay from "./SeasonDisplay";
-import Loader from './Loader';
-import "./style.css";
-class App extends React.Component {
-  state = {
-    latitude: null,
-    errMessage: ""
-  };
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      pos =>
-        this.setState({
-          latitude: pos.coords.latitude
-        }),
-      err =>
-        this.setState({
-          errorMessage: err.message
-        })
-    );
-  }
+// import SeasonDisplay from "./SeasonDisplay";
+// import Loader from './Loader';
+// import "./style.css";
+const App = () => {
 
-  componentDidUpdate() {
-    console.log("component was updated & has re-rendered");
-  }
-
-  renderContent () {
-    if (!this.state.errorMessage && this.state.latitude) {
-      return (
-          <div>
-            <SeasonDisplay latitude={this.state.latitude} />
-          </div>
-      );
-    }
-    if (this.state.errorMessage && !this.state.latitude) {
-      return <div>Error : {this.state.errorMessage}</div>;
-    } else {
-      return <Loader  />
-    }
-  }
-
-  render() {
- return (<div>{this.renderContent()}</div>)
-  }
+  window.navigator.geolocation.getCurrentPosition(
+    pos => console.log(pos),
+    err => console.error(err)
+  )
+  return (
+    <div>
+      <h1>Latitude: </h1>
+      <h1>Longitude: </h1>
+    </div>
+  );
 }
 
 export default App;
