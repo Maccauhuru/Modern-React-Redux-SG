dfebc7984032675dd3fbf5204189fa12699e5e0d
simba <simba3310@gmail.com>
2019-10-02 20:12:47 -0500

    redo seasons app

--- seasonsapp/src/App.js
+++ seasonsapp/src/App.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 // import SeasonDisplay from "./SeasonDisplay";
 // import Loader from './Loader';
 // import "./style.css";
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      lang: null
+    }
+  }
 
-  window.navigator.geolocation.getCurrentPosition(
-    pos => console.log(pos),
-    err => console.error(err)
-  )
-  return (
-    <div>
-      <h1>Latitude: </h1>
-      <h1>Longitude: </h1>
-    </div>
-  );
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      pos => {
+        this.setState({
+          lat: pos.coords.latitude,
+          long: pos.coords.longitude,
+          errMsg: '',
+        })
+      },
+      err => {
+        this.setState({
+          errMsg: err.message
+        })
+      }
+    )
+  }
+
+  render() {
+    if (!this.state.lat && this.state.errMsg) {
+      return (<h1>{this.state.errMsg}</h1>)
+    }
+    if (this.state.lat && !this.state.errMsg) {
+      return (
+        <div>
+          <p>Latitude :{this.state.lat}</p>
+          <p>Longitude :{this.state.long}</p>
+        </div>
+      )
+    }
+    return (<h1>Page loading...</h1>)
+  }
+
 }
 
 export default App;
